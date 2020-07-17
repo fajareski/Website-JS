d74fc73d8575e5cee58faafc57a067903819a9aa
JelajahPackage.id <54346100+fajareski@users.noreply.github.com>
2020-07-17 22:57:22 +0700

    Add files via upload

---
+++ jadwalkelas.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jadwal Kelas</title>
+</head>
+<body>			
+<h1><span style="color: #ff0000;">JADWAL KELAS</span></h1>
+<span style="color: #000080;">BIMBEL PASCASARJANA SIMAK UI 2020</span></p>
+<h1><span style="color: #000080;">Program &amp; Jadwal Bimbel Persiapan SIMAK UI untuk jalur S2 &#8211; Profesi &#8211; PPDS &#8211; Doktor</span></h1>
+			</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+
+<h3 id="fancy-title-150" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				
+<h1><span style="color: #000080;"><span style="color: #c71010;">(Gelombang II &#8211; Ujian 26-30 Juli 2020)</span></span></h1>
+			</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+</div></div></div>		
+</div>
+
+<div class="wpb_row vc_inner vc_row vc_row-fluid   add-padding-0 attched-true   vc_custom_1519566372238 ">
+		
+		<div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner "><div class="wpb_wrapper">
+<h3 id="fancy-title-151" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				PROGRAM HEMAT<br />
+PASTI BISA		</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+
+<div class="mk-fancy-table table-style1  ">
+
+	
+	
+<table>
+<tbody>
+<tr>
+<th>12 SESI FULL DAY<br />
+(2 HARI)</th>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 1 (Rp 1.250.000)</span><br />
+</strong>30 &amp; 31 Mei 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 2 (Rp 1.250.000)</span><br />
+</strong>6 &amp; 7 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 3 (Rp 1.250.000)</span><br />
+</strong>13 &amp; 14 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 4 (Rp 1.250.000)</span><br />
+</strong>20 &amp; 21 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 5 (Rp 1.250.000)</span><br />
+</strong>27 &amp; 28 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 6 (Rp 1.250.000)</span><br />
+</strong>4 &amp; 5 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 7 (Rp 1.250.000)</span><br />
+</strong>11 &amp; 12 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 8 (Rp 1.250.000)</span><br />
+</strong>18 &amp; 19 Jul 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 9 (Rp 1.250.000)</span><br />
+</strong>20 &amp; 21 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 10 (Rp 1.250.000)</span><br />
+</strong>22 &amp; 23 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 11 (Rp 1.250.000)</span><br />
+</strong>24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td>
+<ul>
+<li style="text-align: left;">e-book &amp; e-TO TPA &amp; English di jelajahstudies.com</li>
+<li style="text-align: left;">Waktu Bimbel: 08.00 – maksimal 18.00 WIB dengan total belajar di kelas 18 jam</li>
+<li style="text-align: left;"><strong>1 x Try Out offline </strong> TPA dan English Versi SIMAK UI</li>
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+
+</div>
+</div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner "><div class="wpb_wrapper">
+<h3 id="fancy-title-152" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				PROGRAM INTENSIF<br />
+PASTI BISA			</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+
+<div class="mk-fancy-table table-style1  ">
+
+	
+	
+<table>
+<tbody>
+<tr>
+<th>24 SESI/3 HARI FULL<br />
++ 1 HARI TAMBAHAN</th>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 1 (Rp 2.000.000)</span><br />
+</strong>30, 31 Mei, 6 &amp; 7 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 2 (Rp 2.000.000)</span><br />
+</strong>6, 7, 13, &amp; 14 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 3 (Rp 2.000.000)</span><br />
+</strong>13, 14, 20 &amp; 21 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 4 (Rp 2.000.000)</span><br />
+</strong>20, 21, 27 &amp; 28 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 5 (Rp 2.000.000)</span><br />
+</strong>27, 28 Juni, 4 &amp; 5 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 6 (Rp 2.000.000)</span><br />
+</strong>4, 5, 11 &amp; 12 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 7 (Rp 2.000.000)</span><br />
+</strong>11, 12, 18 &amp; 19 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 8 (Rp 2.000.000)</span><br />
+</strong>18, 19, 20 &amp; 21 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 9 (Rp 2.000.000)</span><br />
+</strong>20, 21, 22 &amp; 23 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 10 (Rp 2.000.000)</span><br />
+</strong>22, 23, 24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td>
+<ul>
+<li style="text-align: left;">e-book &amp; eTO  TPA &amp; English di jelajahstudies.com</li>
+<li style="text-align: left;">Waktu Bimbel: 08.00 – maksimal 18.00 WIB dengan total belajar dikelas 36 jam</li>
+<li style="text-align: left;"><strong>2 x Try Out offline </strong> TPA dan English Versi SIMAK UI</li>
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+
+</div>
+</div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner "><div class="wpb_wrapper">
+<h3 id="fancy-title-153" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				PROGRAM PRIORITY<br />
+PASTI BISA			</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+
+<div class="mk-fancy-table table-style1  ">
+
+	
+	
+<table>
+<tbody>
+<tr>
+<th>48 SESI/8 HARI FULL<br />
+<strong><span style="color: #000000;">+ TAMBAHAN BELAJAR KELOMPOK</span></strong></th>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 1 (Rp 4.000.000)*</span></strong><br />
+30, 31 Mei, 6, 7, 13, 14, 20 &amp; 21 Juni  2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 2 (Rp 4.000.000)*</span></strong><br />
+6, 7, 13, 14, 20, 21, 27 &amp; 28 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 3 (Rp 4.000.000)*</span></strong><br />
+13, 14, 20, 21, 27, 28 Juni, 4 &amp; 5 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 4 (Rp 4.000.000)*</span></strong><br />
+20, 21, 27, 28 Juni, 4, 5, 11 &amp; 12 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 5 (Rp 4.500.000)*</span></strong><br />
+27, 28 Juni, 4, 5, 11, 12, 18 &amp; 19 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 6 (Rp 4.500.000)*</span></strong><br />
+4, 5, 11, 12, 18, 19, 24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 7 (Rp 5.000.000)*</span></strong><br />
+11, 12, 18, 19, 20, 21, 22 &amp; 23 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 8 (Rp 5.000.000)*</span></strong><br />
+11, 12, 18, 19, 22, 23, 24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 9 (Rp 5.000.000)*</span></strong><br />
+18, 19, 20, 21, 22, 23, 24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td>
+<ul>
+<li style="text-align: left;">e-book &amp; e-TO TPA &amp; English di jelajahstudies.com</li>
+<li style="text-align: left;">Waktu Bimbel: 08.00 – maksimal 18.00 WIB dengan total belajar dikelas 72 jam</li>
+<li style="text-align: left;"><strong>4 kali Try Out offline</strong>  TPA dan TOEFL Versi SIMAK UI</li>
+<li style="text-align: left;">Free sesi tambahan belajar perkelompok selasa s.d kamis pkl. 10.00 sd 22.00</li>
+<li style="text-align: left;">Free drilling Psikotes versi UI, Wawancara, MMPI, dan Papi Kostick Test bagi peserta PPDS/PPDGS</li>
+<li style="text-align: left;">Free drilling Focus Group Discussion (FGD) bagi peserta yang mengambil jurusan Psikologi</li>
+<li style="text-align: left;">Free drilling penyusunan motivation letter &amp; academic CV yang tepat</li>
+<li style="text-align: left;">*Cashback sebesar Rp1.500.000, jika tidak lulus  sesuai dengan syarat &amp; ketentuan yang berlaku, yaitu Wajib hadir ontime dan penuh di kelas  dan mengerjakan tugas dengan lengkap</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+
+<div class="wpb_row vc_inner vc_row vc_row-fluid   add-padding-0 attched-true   vc_custom_1519566372238 ">
+		
+		<div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner "><div class="wpb_wrapper">
+<h3 id="fancy-title-151" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				PROGRAM PRIVATE<br />
+PASTI BISA		</span>
+</h3>
+<div class="clearboth"></div>
+
+
+
+
+<div class="mk-fancy-table table-style1  ">
+
+	
+	
+<table>
+<tbody>
+<tr>
+<th>12 SESI FULL DAY<br />
+(2 HARI)</th>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 1 (Rp 1.250.000)</span><br />
+</strong>30 &amp; 31 Mei 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #000080;">Gelombang 2 (Rp 1.250.000)</span><br />
+</strong>6 &amp; 7 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 3 (Rp 1.250.000)</span><br />
+</strong>13 &amp; 14 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 4 (Rp 1.250.000)</span><br />
+</strong>20 &amp; 21 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 5 (Rp 1.250.000)</span><br />
+</strong>27 &amp; 28 Juni 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 6 (Rp 1.250.000)</span><br />
+</strong>4 &amp; 5 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 7 (Rp 1.250.000)</span><br />
+</strong>11 &amp; 12 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 8 (Rp 1.250.000)</span><br />
+</strong>18 &amp; 19 Jul 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 9 (Rp 1.250.000)</span><br />
+</strong>20 &amp; 21 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 10 (Rp 1.250.000)</span><br />
+</strong>22 &amp; 23 Juli 2020</td>
+</tr>
+<tr>
+<td><strong><span style="color: #333399;">Gelombang 11 (Rp 1.250.000)</span><br />
+</strong>24 &amp; 25 Juli 2020</td>
+</tr>
+<tr>
+<td>
+<ul>
+<li style="text-align: left;">e-book &amp; e-TO TPA &amp; English di jelajahstudies.com</li>
+<li style="text-align: left;">Waktu Bimbel: 08.00 – maksimal 18.00 WIB dengan total belajar di kelas 18 jam</li>
+<li style="text-align: left;"><strong>1 x Try Out offline </strong> TPA dan English Versi SIMAK UI</li>
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+
+</div>
+</div>
+</div></div></div>		
+</div>
+<h3 id="fancy-title-154" class="mk-fancy-title  simple-style  color-single">
+	<span>
+				
+</body>
+</html>
